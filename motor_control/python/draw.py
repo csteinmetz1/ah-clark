722ff4cc0b10ea87d9c80d5dcd4b4eb05344bbc2
csteinmetz1 <csteinmetz1@gmail.com>
2018-10-03 13:28:22 -0400

    woo - finally got the kinematics and plotting of them to work correctly - still isues with the alternate solution

--- motor_control/python/draw.py
+++ motor_control/python/draw.py
@@ -46,43 +46,61 @@ class TivaController:
         x += self.x_offset
         y += self.y_offset
 
-        q2 = np.arccos((x**2 + y**2 - self.a1**2 - self.a2**2) / (2 * self.a1 * self.a2))
+        q2 = np.arccos((x**2 + y**2 - self.a1**2 - self.a2**2) / (2.0 * self.a1 * self.a2))
         
-        if flip:
+        if flip: # this solution isn't working currently
             q2 = -q2
             q1 = np.arctan2(y, x) + np.arctan2( (self.a2 * np.sin(q2)), (self.a1 + (self.a2 * np.cos(q2))) )
         else:
             q1 = np.arctan2(y, x) - np.arctan2( (self.a2 * np.sin(q2)), (self.a1 + (self.a2 * np.cos(q2))) )
             
-        return q1*(180/np.pi), q2*(180/np.pi)
+        return q1, q2
+    
+    def joints_to_hand(self, q1,q2):
+        x1 = self.a1 * np.cos(q1)
+        y1 = self.a1 * np.sin(q1)
+        x2 = x1 + (self.a2 * np.cos(q1+q2))
+        y2 = y1 + (self.a2 * np.sin(q1+q2))
+
+        return x1, y1, x2, y2
 
 if __name__ == "__main__":
-    Tiva = TivaController(units_per_cm=1, arm1_cm=20, arm2_cm=10, x_offset_cm=0, y_offset_cm=0, bufsize=8) # set up comm channels
+    Tiva = TivaController(units_per_cm=1, arm1_cm=10, arm2_cm=10, x_offset_cm=0, y_offset_cm=0, bufsize=8) # set up comm channels
     
     # draw a circle
     n_steps = 25
-    r = 15.0
-    u = np.linspace(0, 2*np.pi, num=n_steps)
-    x = r * np.cos(u)
-    y = r * np.sin(u)
+    r = 10
+    u = np.linspace(0, np.pi, num=n_steps)
+    x = r * np.cos(u) + 0
+    y = r * np.sin(u) + 5
+
+    #x = np.linspace(-5, 5, 25)
+    #y = np.linspace(10, 12, 25)
+
+    #x = [1]
+    #y = [1]
+
+    fig, ax = plt.subplots()
 
     for idx, point in enumerate(zip(x, y)):
 
+        #print(point[0], point[1])
         q1, q2 = Tiva.compute_kinematics(point[0], point[1])
-        print(q1, q2)
+        _x1, _y1, _x2, _y2 = Tiva.joints_to_hand(q1, q2)
 
-        _x1 = Tiva.a1 * np.cos(q1)
-        _y1 = Tiva.a1 * np.sin(q1)
-        _x2 = Tiva.a2 * np.cos(q2)
-        _y2 = Tiva.a2 * np.sin(q2)
+        print("{0}: {1:3.3f} {2:3.3f} | {3:3.3f}".format(idx, q1*(180/np.pi), q2*(180/np.pi), q1*(180/np.pi)+q2*(180/np.pi)))
 
-        print(_x2, _y2)
+        if np.isnan(q1) or np.isnan(q2):
+            break
 
         x1, y1 = [0, _x1], [0, _y1]
         x2, y2 = [_x1, _x2], [_y1, _y2]
-        plt.plot(x1, y1, x2, y2, marker = 'o')
-        plt.xlim(-20, 20)
-        plt.ylim(-20, 20)
-        plt.show()
+
+        ax.plot(x1, y1, marker = 'o', color='b')
+        ax.plot(x2, y2, marker = 'o', color='g')
+        ax.plot(point[0], point[1], marker = 'o', color='r')
+        plt.axis('equal')
+        plt.savefig("img/{}.png".format(idx))
+        
         #comm.send([q1, q2])
 
