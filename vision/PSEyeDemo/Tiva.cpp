7e808476f1d4f090da9ceda29bb751b3730b4059
csteinmetz1 <csteinmetz1@gmail.com>
2018-10-06 19:56:44 -0400

    adding some comments and clean up for Tiva class

--- vision/PSEyeDemo/Tiva.cpp
+++ vision/PSEyeDemo/Tiva.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class TivaController 
 {
 	public:
-
+		// getter methods
 		double getMotor1Angle() const {return q1;}
 		double getMotor2Angle() const {return q2;}
 		tuple<double, double> getArm1Location() const {return make_tuple(x1, y1);}
@@ -18,44 +18,37 @@ class TivaController
 		double getxOffset() const {return xOffset;}
 		double getyOffset() const {return yOffset;}
 
-
+		// setter methods
 		void setMotor1Angle(double new_q1) { q1 = new_q1; updateArmLocation();}
 		void setMotor2Angle(double new_q2) { q2 = new_q2; updateArmLocation();}
-		// excluding setter for offsets and arm lengths at the moment
-		// user cannot control arm by adjusting x1,y1 x2,y2 points
+		void setXOffsetCm(double new_xOffsetCm) {xOffset = new_xOffsetCm * unitsPerCm;}
+		void setYOffsetCm(double new_yOffsetCm) {xOffset = new_yOffsetCm * unitsPerCm;}
+		void setArm1Cm(double new_arm1Cm) {a1 = new_arm1Cm * unitsPerCm;}
+		void setArm2Cm(double new_arm2Cm) {a2= new_arm2Cm * unitsPerCm;}
 
-
-		void computeKinematics(bool negative) 
-		{ 
-			// negative q2 solution
-        	double q2_neg = -(acos((x**2 + y**2 - self.a1**2 - self.a2**2) / (2.0 * self.a1 * self.a2)))
-        	double q1_neg = np.atan2(y, x) - np.atan2( (self.a2 * np.sin(q2_neg)), (self.a1 + (self.a2 * np.cos(q2_neg))) )
-
-			// postive q2 solution
-			double q2_pos = np.arccos((x**2 + y**2 - self.a1**2 - self.a2**2) / (2.0 * self.a1 * self.a2))
-			double q1_pos = np.arctan2(y, x) - np.arctan2( (self.a2 * np.sin(q2_pos)), (self.a1 + (self.a2 * np.cos(q2_pos))) )
-		}
-
-		void updateArmLocation() 
-		{
-			x1 = a1 * cos(q1) + xOffset;
-			y1 = a1 * sin(q1) + yOffset;
-			x2 = x1 + (a2 * cos(q1+q2));
-			y2 = y1 + (a2 * sin(q1+q2));
-		}
-
-		void resetArm() { setMotor1Angle(0.0); setMotor2Angle(0.0);};
-		void moveArm(double x, double y);
-
+		// arm movement methods
+		void moveArm(double x, double y, bool negative);
+		void resetArm();
+		void updateArmLocation();
+		tuple<double, double> computeKinematics(double x, double y, bool negative);
 
 		// Constructor
-		TivaController(double unitsPerCm, double arm1Cm, double arm2Cm, 
+		TivaController(double _unitsPerCm, double arm1Cm, double arm2Cm, 
 					   double xOffsetCm, double yOffsetCm)
 		{
+			unitsPerCm = _unitsPerCm;
 			xOffset = unitsPerCm * xOffsetCm;
 			yOffset = unitsPerCm * yOffsetCm;
 			a1 = unitsPerCm * arm1Cm;
 			a2 = unitsPerCm * arm2Cm;
+
+			// initialize arm 
+			q1 = 0.0;
+			q2 = 0.0;
+			x1 = 0.0;
+			y1 = 0.0;
+			x2 = 0.0;
+			y2 = 0.0;
 		};
 	
 	private:
@@ -66,25 +59,61 @@ class TivaController
 		double x2;
 		double y2;
 
+		double unitsPerCm;
 		double xOffset;
 		double yOffset;
 		double a1;
 		double a2;
 };
 
-void TivaController::resetArm(void) {
-	//arm_position.q1 = 0.0;
-	//arm_position.q2 = 0.0;
+void TivaController::resetArm(void) 
+{
+	setMotor1Angle(0.0);
+	setMotor2Angle(0.0);
+	updateArmLocation();
 };
 
-void TivaController::moveArm(double x, double y) {
-
-	//double x_rel -= x_offset_cm;
+void TivaController::moveArm(double x, double y, bool negative) 
+{
+	tuple<double, double> newAngles;
+	newAngles = computeKinematics(x, y, negative);
+	setMotor1Angle(get<0>(newAngles));
+	setMotor2Angle(get<1>(newAngles));
+	updateArmLocation();
 
 };
 
+tuple<double, double> TivaController::computeKinematics(double x, double y, bool negative) 
+{ 
+	double new_q1, new_q2;
+
+	x -= xOffset;
+	y -= yOffset;
+
+	if (negative) // negative q2 solution
+	{
+		new_q2 = -(acos(((pow(x,2) + pow(y,2) - pow(a1,2) - pow(a2,2)) / (2.0 * a1 * a2))));
+		new_q1 = atan2(y, x) - atan2((a2 * sin(new_q2)), (a1 + (a2 * cos(new_q2))));
+	}
+	else 		 // positive q2 solution
+	{
+		new_q2 =  acos( (pow(x,2) + pow(y,2) - pow(a1,2) - pow(a2,2)) / (2.0 * a1 * a2) );
+		new_q1 = atan2(y, x) - atan2((a2 * sin(new_q2)), (a1 + (a2 * cos(new_q2))));
+	}
+	return make_tuple(new_q1, new_q2);
+}
+
+void TivaController::updateArmLocation() 
+{
+	x1 = a1 * cos(q1) + xOffset;
+	y1 = a1 * sin(q1) + yOffset;
+	x2 = x1 + (a2 * cos(q1+q2));
+	y2 = y1 + (a2 * sin(q1+q2));
+}
 
 int main() {
-	TivaController Tiva = TivaController(1, 45.0, 29.0, 0.0, 0.0);
+	TivaController Tiva = TivaController(1.0, 45.0, 20.0, 33.0, -10.0);
+	cout << Tiva.getMotor1Angle() << "\n";
+	Tiva.moveArm(5.0, 20.0, false);
 	cout << Tiva.getMotor1Angle() << "\n";
 }
