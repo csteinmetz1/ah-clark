512ba82061ae2fb520da5e7455f58f91f5099817
dhastin <43764287+dhastin@users.noreply.github.com>
2018-10-14 18:36:32 -0400

    Add files via upload
    Up to Date UDP Tiva related code.  Christian look at these and the other file structure at the same time

---
+++ UDP_FullDuplexS9/UDP_FullDuplexS9/Image_Proc.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "stdafx.h"
+#include <Windows.h>
+#include <iostream>
+#include <iomanip>
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+using namespace cv;
+using namespace std;
+
+void noise_reduction(Mat, Mat);
+void puck_location(Mat, Moments, double *, double *, double *, double *, double *);
+
+
