2bfe14b504340034e69644f8c394c92a686ded04
csteinmetz1 <csteinmetz1@gmail.com>
2018-11-28 14:17:23 -0500

    slight update that predicts the target in a better way

--- clark/PSEyeDemo/Tiva.cpp
+++ clark/PSEyeDemo/Tiva.cpp
@@ -335,8 +335,12 @@ std::vector<Vec_double> TivaController::computeBlockAndHitPath(std::vector<Vec_d
 	double slope; 			  // slope of the line connecting puck hit point and puck target point
 	Vec_double hitEndPoint;   // end point of the hit path which is past the puck on the line towards the target
 
+
+	// point on the the trajectory before the hit point
+	if ()
+
 	// determine targetPoint to based on hit point to bank off wall
-	if (blockPoint.x <= 33)
+	if (trajectory.end()[-2].x < blockPoint.x)
 	{
 		targetPoint.x = 0.0;
 		targetPoint.y = 75.0;
